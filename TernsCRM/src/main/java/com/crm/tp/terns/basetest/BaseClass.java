6a7462c74b6bc918b18d51ace70d8a2a984c3f70
shubh <shubh@192.168.1.168>
2024-11-13 22:04:05 +0530

    hello

--- TernsCRM/src/main/java/com/crm/tp/terns/basetest/BaseClass.java
+++ TernsCRM/src/main/java/com/crm/tp/terns/basetest/BaseClass.java
@@ -13,7 +13,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+
 
 import com.crm.tp.terns.databaseutility.DataBaseUtility;
 import com.crm.tp.terns.excelutility.ExcelUtility;
